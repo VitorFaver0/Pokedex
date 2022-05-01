ace0a25885b46208f415f50ce711a6d7d6b2d626
VitorFaver0 <88974981+VitorFaver0@users.noreply.github.com>
2022-05-01 17:12:58 -0300

    correção 2

--- src/pages/pokemonInfo.tsx
+++ src/pages/pokemonInfo.tsx
@@ -14,18 +14,32 @@ type Params = {
 }
 
 const PokemonInfo: React.FC = (props:Params) =>{
-    const pokemon = JSON.parse(props.router.query.pokemon);
+    try {
+        const pokemon = JSON.parse(props.router.query.pokemon);
 
-    return (
-        <Container>
-            <Head>
-                <title>Pokedex</title>
-                <link rel="icon" href="favicon.ico" />
-            </Head>
-            <NavBar exibirMenu={false}></NavBar>
-            <CardPokemon pokemon={pokemon}></CardPokemon>
-        </Container>
-    )
+        return (
+            <Container>
+                <Head>
+                    <title>Pokedex</title>
+                    <link rel="icon" href="favicon.ico" />
+                </Head>
+                <NavBar exibirMenu={false}></NavBar>
+                <CardPokemon pokemon={pokemon}></CardPokemon>
+            </Container>
+        )
+    } catch (error) {
+        return (
+            <Container>
+                <Head>
+                    <title>Pokedex</title>
+                    <link rel="icon" href="favicon.ico" />
+                </Head>
+                <NavBar exibirMenu={false}></NavBar>
+                <h1>Loading...</h1>
+            </Container>
+        )
+    }
+
 
 }
 
